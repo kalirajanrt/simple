1f863462d4267841300b12389cd787e3e2daef11
kalirajanrt <kalirajanrt@gmail.com>
2017-12-13 14:41:31 +0530

    first commit , uploading all files

---
+++ Simple/src/Addition.java
@@ -0,0 +1,20 @@
+
+public class Addition {
+	
+	int a;
+	int b;
+	
+	Addition(int a, int b){
+		this.a = a;
+		this.b = b;
+		
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		
+		Addition ad = new Addition(3, 5);
+		
+	}
+	
+}
